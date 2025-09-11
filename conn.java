5ea4a91077003037e336277ec4082bbf26e515d6
Sooraj K R <krsooraj465@gmail.com>
2025-09-11 23:35:26 +0530

    Add files via upload
    Uploaded Login page and Connection with sql code, needs changes with further code additions

---
+++ conn.java
@@ -0,0 +1,23 @@
+package hostpital.management.system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class conn {
+    Connection connection;
+    Statement statement;
+
+    public conn() {
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/hospital_management_system",
+                    "root",
+                    "soorajkr@4209"
+            );
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
